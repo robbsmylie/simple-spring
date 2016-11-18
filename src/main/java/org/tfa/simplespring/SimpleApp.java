55fb72a7f38cc30223a6194e2be8333092d511d2
rsmylie <robb.smylie@teachforamerica.org>
2016-11-18 10:24:09 -0500

    initial commit

---
+++ src/main/java/org/tfa/simplespring/SimpleApp.java
@@ -0,0 +1,42 @@
+package org.tfa.simplespring;
+
+//import javax.security.auth.message.config.AuthConfigFactory;
+//import org.apache.catalina.authenticator.jaspic.AuthConfigFactoryImpl;
+
+import org.springframework.boot.*;
+import org.springframework.boot.autoconfigure.*;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.*;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@SpringBootApplication
+public class SimpleApp extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+    	
+    	//if (AuthConfigFactory.getFactory() == null) {
+        //    AuthConfigFactory.setFactory(new AuthConfigFactoryImpl());
+    	//}
+    	return application.sources(SimpleApp.class);
+    }
+
+    public static void main(String[] args) throws Exception {
+    	
+    	//if (AuthConfigFactory.getFactory() == null) {
+        //    AuthConfigFactory.setFactory(new AuthConfigFactoryImpl());
+        //}
+    	
+        SpringApplication.run(SimpleApp.class, args);
+    }
+ 
+    @RequestMapping("/")
+    String home() {
+        System.out.println("/ called, returning Hello World");
+        return "Hello World!";
+    }
+}
